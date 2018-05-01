44d20695d92c31d54963683506bb7022e43206f7
David Smith <smith@opennms.com>
2018-05-01 13:33:53 -0400

    Stub Group implementation

---
+++ model/impl/src/main/java/org/opennms/oce/model/impl/GroupImpl.java
@@ -0,0 +1,56 @@
+package org.opennms.oce.model.impl;
+
+import org.opennms.oce.model.api.Group;
+import org.opennms.oce.model.api.ModelObject;
+
+public class GroupImpl implements Group {
+
+    @Override
+    public ModelObject getOwner() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getType() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getNumberMembers() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getNumberNormalState() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getNumberNonServiceAffecting() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getNumberServiceAffecting() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public String getNumber() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String friendlyName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}

eece82972862ba13476b4c279c4eae6f06367eff
Jesse White <jesse@opennms.org>
2018-05-01 14:25:59 -0400

    Revise XML, define an XSD and generate JAXB model objects from the XSD.

---
+++ model/impl/src/main/java/org/opennms/oce/model/impl/Main.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2018 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2018 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.oce.model.impl;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.XMLConstants;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+
+import org.opennms.oce.model.v1.schema.Inventory;
+import org.opennms.oce.model.v1.schema.MetaModel;
+import org.xml.sax.SAXException;
+
+public class Main {
+
+    public static void main(String[] args) throws JAXBException, SAXException, IOException {
+        final SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        final Schema schema = sf.newSchema( Main.class.getResource("/model.xsd"));
+
+        final MetaModel metaModel;
+        try (InputStream is = Main.class.getResourceAsStream("/metamodel.v1.xml")) {
+            final JAXBContext ctx = JAXBContext.newInstance(MetaModel.class);
+            final Unmarshaller unmarshaller = ctx.createUnmarshaller();
+            unmarshaller.setSchema(schema);
+            metaModel = (MetaModel) unmarshaller.unmarshal(is);
+        }
+        System.out.println(metaModel);
+
+        final Inventory inventory;
+        try (InputStream is = Main.class.getResourceAsStream("/inventory.v1.xml")) {
+            final JAXBContext ctx = JAXBContext.newInstance(Inventory.class);
+            final Unmarshaller unmarshaller = ctx.createUnmarshaller();
+            unmarshaller.setSchema(schema);
+            inventory = (Inventory) unmarshaller.unmarshal(is);
+        }
+        System.out.println(inventory);
+    }
+}

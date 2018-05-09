1d562cddd50cde11158c47e1677f27e71ee73020
Jesse White <jesse@opennms.org>
2018-05-09 16:17:47 -0400

    Basic layout for cluster engine.

---
+++ engine/cluster/src/main/java/org/opennms/oce/engine/cluster/ClusterEngine.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2018 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2018 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.oce.engine.cluster;
+
+import org.opennms.oce.engine.api.Engine;
+import org.opennms.oce.engine.api.IncidentHandler;
+import org.opennms.oce.model.alarm.api.Alarm;
+import org.opennms.oce.model.api.Model;
+
+public class ClusterEngine implements Engine {
+    @Override
+    public void onAlarm(Alarm alarm) {
+
+    }
+
+    @Override
+    public void setInventory(Model inventory) {
+
+    }
+
+    @Override
+    public void registerIncidentHandler(IncidentHandler handler) {
+
+    }
+}

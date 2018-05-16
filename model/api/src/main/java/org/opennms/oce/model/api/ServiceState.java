147a4b725f03d15ee1db25b6921793d86ca460e5
David Smith <smith@opennms.com>
2018-05-16 09:29:14 -0400

    Updating ServiceState per Confluence

--- model/api/src/main/java/org/opennms/oce/model/api/ServiceState.java
+++ model/api/src/main/java/org/opennms/oce/model/api/ServiceState.java
@@ -1,5 +1,13 @@
 package org.opennms.oce.model.api;
 
 public enum ServiceState {
-    IN, OUT
+    @Deprecated
+    IN,
+    @Deprecated
+    OUT,
+    UNDEFINED,
+    IN_MAINTENANCE,
+    COMMISSIONING,
+    IN_SERVICE,
+    OUT_OF_SERVICE,
 }

35a3fe9a148daf228d2c6cb4dfdde0bcc89e1e1b
David Smith <smith@opennms.com>
2018-11-15 15:41:19 -0500

    OCE-19: Some rendering style cleanup

--- features/graph/common/src/main/resources/overlay/etc/graphml-edge-status/oce.groovy
+++ features/graph/common/src/main/resources/overlay/etc/graphml-edge-status/oce.groovy
@@ -1,18 +1,21 @@
 import org.opennms.netmgt.model.OnmsSeverity;
 import org.opennms.features.topology.plugins.topo.graphml.status.GraphMLEdgeStatus;
 
+
+String opacity = edge.properties.garbage_collected != null && edge.properties.garbage_collected.equals("true") ? "0.1" : "1.0"
+
 if (edge.properties.type == "situation") {
   return new GraphMLEdgeStatus(OnmsSeverity.CRITICAL, [ 
-    'stroke-dasharray' : '5,5', 
     'stroke' : 'indigo',
+    'stroke-opacity' : opacity ,
     'stroke-width' : "" + edge.properties.edgeWeight / 2
   ]);
 }
 
 else if (edge.properties.type == "inventory") {
   return new GraphMLEdgeStatus(OnmsSeverity.CRITICAL, [ 
-    'stroke-dasharray' : '5,5', 
     'stroke' : 'blue',
+    'stroke-opacity' : opacity ,
     'stroke-width' : "" + edge.properties.edgeWeight / 2
   ]);
 }
@@ -26,7 +29,6 @@ else if (edge.properties.edgeWeight != null ) {
 }
 
 return new GraphMLEdgeStatus(OnmsSeverity.CRITICAL, [ 
-  'stroke-dasharray' : '5,5', 
   'stroke' : 'gray',
   'stroke-width' : '1'
 ]);

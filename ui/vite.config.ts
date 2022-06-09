2e31fd9ca242783818ff3bcbd0e7c8dadad50a9c
Anya <anya.rybalova@gmail.com>
2022-06-09 16:27:15 -0400

    jw/newui - fixed tsconfig and vite.config

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
 	build: {
 		cssCodeSplit: false, // keep css in one chunk
 		lib: {
-			entry: path.resolve(__dirname, 'src/main.ts'),
+			entry: new URL('src/main.ts', import.meta.url).pathname,
 			name: 'uiextension',
 			fileName: (format) => `uiextension.${format}.js`
 		},

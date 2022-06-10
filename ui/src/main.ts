4886a5b931262114a455ea13a1fbd5aa8c70dbcf
Anya <anya.rybalova@gmail.com>
2022-06-10 15:48:28 -0400

    ALEC-124 - alec welcome page

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,7 +1,9 @@
 import App from './App.vue'
 import { createApp } from 'vue'
 const envMode = import.meta.env.MODE
-
+import '@featherds/styles'
+import '@featherds/styles/themes/open-light.css'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-ignore
 window['uiextension'] = App
 

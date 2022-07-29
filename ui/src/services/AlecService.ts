c3db51acea76e38a2bc277b1f6cbf282f870d999
Anya <anya.rybalova@gmail.com>
2022-07-29 10:05:34 -0400

    ALEC-159 - feedback situation

--- ui/src/services/AlecService.ts
+++ ui/src/services/AlecService.ts
@@ -1,11 +1,12 @@
 import { rest } from './axiosInstances'
 import CONST from '@/helpers/constants'
+const base = '/alec'
 const engineEndpoint = '/alec/engine/configuration'
-const endpoint = '/alec/agreement/configuration'
+const endpointAgreement = '/alec/agreement/configuration'
 
 export const savePermission = async (allowSaveValue: boolean) => {
 	try {
-		const resp = await rest.post(`${endpoint}`, {
+		const resp = await rest.post(`${endpointAgreement}`, {
 			agreed: allowSaveValue
 		})
 		if (resp.status === 201) {
@@ -19,7 +20,7 @@ export const savePermission = async (allowSaveValue: boolean) => {
 
 export const getUserInfo = async () => {
 	try {
-		const resp = await rest.get(`${endpoint}`)
+		const resp = await rest.get(`${endpointAgreement}`)
 		if (resp.status === 200) {
 			return resp.data
 		}
@@ -45,6 +46,25 @@ export const saveEngineParameter = async (
 			alpha: 144.47117699,
 			beta: 0.55257784,
 			epsilon: 100
+		})
+		if (resp.status === 200) {
+			return true
+		}
+		return false
+	} catch (err) {
+		return false
+	}
+}
+
+export const sendFeedbackAcceptSituation = async (
+	id: string,
+	action: string
+) => {
+	try {
+		const resp = await rest.post(`${base}/situation/${action}/${id}`, {
+			headers: {
+				'Access-Control-Allow-Origin': '*'
+			}
 		})
 		if (resp.status === 200) {
 			return true
--- ui/src/store/useSituationsStore.ts
+++ ui/src/store/useSituationsStore.ts
@@ -1,25 +1,35 @@
 import { defineStore } from 'pinia'
 import { getSituations } from '@/services/AlarmService'
-import { TSituation } from '@/types/TSituation'
+import { TAlarm, TRelatedAlarm, TSituation } from '@/types/TSituation'
+import { mapKeys } from 'lodash'
 
 type TState = {
 	situations: TSituation[]
+	alarms: Record<string, TAlarm>
 }
 
 export const useSituationsStore = defineStore('situationsStore', {
 	state: (): TState => ({
-		situations: []
+		situations: [],
+		alarms: {}
 	}),
 	actions: {
 		async getSituations() {
 			const result = await getSituations()
 			if (result) {
-				this.situations = processAlarmList(result.alarm)
+				this.alarms = mapKeys(result.alarm, (value) => {
+					return value.id
+				})
+				const situations = result.alarm.filter(
+					(a) => a.relatedAlarms && a.relatedAlarms.length > 0
+				)
+				situations.forEach((sit) => {
+					sit.relatedAlarms.forEach((alarm: TRelatedAlarm) => {
+						alarm.count = this.alarms[alarm.id]?.count
+					})
+				})
+				this.situations = situations
 			}
 		}
 	}
 })
-
-const processAlarmList = (alarms: TSituation[]) => {
-	return alarms.filter((a) => a.relatedAlarms && a.relatedAlarms.length > 0)
-}

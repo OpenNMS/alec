337121b12f8aa3977db676802812b75723b21936
Anya <anya.rybalova@gmail.com>
2022-09-01 14:06:35 -0400

    ALEC-179 - tests for tabs

---
+++ ui/src/tests/situationDetail.test.ts
@@ -0,0 +1,28 @@
+import { test, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import SituationDetailTab from '@/components/SituationDetailTab.vue'
+import { createTestingPinia } from '@pinia/testing'
+import { situationsMock } from './Mock/situationsMock'
+const situation = situationsMock[0]
+
+const wrapper = mount(SituationDetailTab, {
+	global: {
+		plugins: [createTestingPinia()],
+		props: { situation }
+	}
+} as any) as any
+
+test('Should emit status and situation Id on click', async () => {
+	const btnAccept = wrapper.find('[data-test="btn-accept"]')
+	await btnAccept.trigger('click')
+	expect(wrapper.emitted()['situation-status-changed'][0]).toEqual([
+		'ACCEPTED',
+		situation.id
+	])
+})
+
+test('Should change situation status on click', async () => {
+	const btnAccept = wrapper.find('[data-test="btn-accept"]')
+	await btnAccept.trigger('click')
+	expect(wrapper.vm.status).toEqual('ACCEPTED')
+})

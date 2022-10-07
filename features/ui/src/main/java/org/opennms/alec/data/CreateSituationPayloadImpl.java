a0af8383e303633d0444f390d9cb307c6c88db45
Benjamin Janssens <bjanssens@opennms.com>
2022-10-07 16:01:25 -0400

    Add endpoint to create situation from alarm id list
    ALEC-194

---
+++ features/ui/src/main/java/org/opennms/alec/data/CreateSituationPayloadImpl.java
@@ -0,0 +1,99 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2022 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2022 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.alec.data;
+
+import java.util.List;
+
+public class CreateSituationPayloadImpl implements CreateSituationPayload {
+
+    private final List<String> alarmIdList;
+    private final String diagnosticText;
+    private final String description;
+
+    private CreateSituationPayloadImpl(Builder builder) {
+        alarmIdList = builder.alarmIdList;
+        diagnosticText = builder.diagnosticText;
+        description = builder.description;
+    }
+
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    public static Builder newBuilder(CreateSituationPayloadImpl copy) {
+        Builder builder = new Builder();
+        builder.alarmIdList = copy.getAlarmIdList();
+        builder.diagnosticText = copy.getDiagnosticText();
+        builder.description = copy.getDescription();
+        return builder;
+    }
+
+    public static final class Builder {
+        private List<String> alarmIdList;
+        private String diagnosticText;
+        private String description;
+
+        private Builder() {
+        }
+
+        public Builder alarmIdList(List<String> val) {
+            alarmIdList = val;
+            return this;
+        }
+
+        public Builder diagnosticText(String val) {
+            diagnosticText = val;
+            return this;
+        }
+
+        public Builder description(String val) {
+            description = val;
+            return this;
+        }
+
+        public CreateSituationPayloadImpl build() {
+            return new CreateSituationPayloadImpl(this);
+        }
+    }
+
+    @Override
+    public List<String> getAlarmIdList() {
+        return alarmIdList;
+    }
+
+    @Override
+    public String getDiagnosticText() {
+        return diagnosticText;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+}
